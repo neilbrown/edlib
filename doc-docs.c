0ae0632b42ed59e91ecfd78e29c45bb68a8d7ce4
NeilBrown <neil@brown.name>
2016-04-21 17:27:51 +1000

    docs:show-modified: check if anything is modified at start.
    That way "exit" just works if nothing is modified.
    
    Signed-off-by: NeilBrown <neil@brown.name>

--- doc-docs.c
+++ doc-docs.c
@@ -327,6 +327,8 @@ DEF_CMD(docs_callback)
 		struct pane *p = doc_attach_view(ci->focus, doc->doc.home, NULL);
 		p = pane_register(pane_final_child(p), 0, &docs_modified_handle, doc, NULL);
 		call3("Request:Notify:Replace", p, 0, NULL);
+		/* And trigger Notify:Replace handling immediately...*/
+		call3("Notify:Replace", p, 0, NULL);
 		return 1;
 	}
 
