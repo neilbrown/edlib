neilbrown/edlib

commit 33b9096153ebb93f641078a1c58e067884c3efb2

doc-docs: use doc:mymark to avoid using the wrong mark.
Have doc_new_mark() add to the list so that docs_set_ref works.

Signed-off-by: NeilBrown <neil@brown.name>

core-mark.c
@@ -403,6 +403,7 @@ struct mark *doc_new_mark(struct doc *d safe, int view)
 	INIT_HLIST_NODE(&ret->all);
 	INIT_TLIST_HEAD(&ret->view, GRP_MARK);
 	ret->viewnum = view;
+	hlist_add_head(&ret->all, &d->marks);
 	mark_reset(d, ret, 0);
 	if (hlist_unhashed(&ret->all)) {
 		/* Document misbehaved, fail gracefully */

doc-docs.c
@@ -428,6 +428,8 @@ DEF_CMD(docs_step)
 
 	if (!m)
 		return -1;
+	if (call3("doc:mymark", ci->home, 0, ci->mark) != 1)
+		return -1;
 
 	p = m->ref.p;
 	if (forward) {
@@ -483,6 +485,8 @@ DEF_CMD(docs_set_ref)
 
 	if (!m)
 		return -1;
+	if (call3("doc:mymark", ci->home, 0, ci->mark) != 1)
+		return -1;
 
 	if (ci->numeric == 1 && !list_empty(&d->doc.home->children))
 		m->ref.p = list_first_entry(&d->doc.home->children,
@@ -498,6 +502,10 @@ DEF_CMD(docs_set_ref)
 
 DEF_CMD(docs_mark_same)
 {
+	if (call3("doc:mymark", ci->home, 0, ci->mark) != 1)
+		return -1;
+	if (call3("doc:mymark", ci->home, 0, ci->mark2) != 1)
+		return -1;
 	if (!ci->mark || !ci->mark2)
 		return -1;
 	return ci->mark->ref.p == ci->mark2->ref.p ? 1 : 2;
@@ -540,6 +548,8 @@ DEF_CMD(docs_doc_get_attr)
 	char *val;
 
 	if (!m || !attr)
+		return -1;
+	if (call3("doc:mymark", ci->home, 0, ci->mark) != 1)
 		return -1;
 
 	val = __docs_get_attr(d, m, forward, attr);
@@ -726,6 +736,8 @@ DEF_CMD(docs_cmd)
 	char cmd;
 
 	if (!ci->str)
+		return -1;
+	if (call3("doc:mymark", ci->home, 0, ci->mark) != 1)
 		return -1;
 	cmd = ci->str[0];
 	switch(cmd) {

commit 6a54614517d071a815acaf1315b04dac488e0acc

Fix problem caused where I started using doc:mymark more
I need to insert in a list before calling mark_reset()
from *both* places that a mark is created.

Fixes: 33b9096153eb ("doc-docs: use doc:mymark to avoid using the wrong mark.")
Signed-off-by: NeilBrown <neil@brown.name>

core-mark.c
@@ -331,6 +331,7 @@ struct mark *safe point_new(struct doc *d safe)
 	INIT_TLIST_HEAD(&ret->view, GRP_MARK);
 	ret->attrs = NULL;
 	ret->viewnum = MARK_POINT;
+	hlist_add_head(&ret->all, &d->marks);
 	ret->mdata = lnk;
 	ret->mtype = NULL;
 	lnk->size = d->nviews;

doc-docs.c
@@ -485,7 +485,7 @@ DEF_CMD(docs_set_ref)
 
 	if (!m)
 		return -1;
-	if (call3("doc:mymark", ci->home, 0, ci->mark) != 1)
+	if (call3("doc:mymark", ci->home, 0, m) != 1)
 		return -1;
 
 	if (ci->numeric == 1 && !list_empty(&d->doc.home->children))