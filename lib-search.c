cd17d26f332c924d10b7853b87d671f15e9960e5
NeilBrown <neil@brown.name>
2017-09-22 03:21:17 +0200

    lib-search: add backwards-search option.
    This doesn't seems as efficient, but that probably isn't important
    
    Signed-off-by: NeilBrown <neil@brown.name>

--- lib-search.c
+++ lib-search.c
@@ -42,6 +42,39 @@ static int search_forward(struct pane *p safe, struct mark *m safe, struct mark 
 	return since_start;
 }
 
+static int search_backward(struct pane *p safe, struct mark *m safe, struct mark *m2,
+			   unsigned short *rxl safe,
+			   struct mark *endmark safe)
+{
+	/* Search backward from @m in @p for a match of @s.  The match must start
+	 * before m, but may finish later.
+	 * Only search as far as @m2 (if set), and leave endmark pointing at the
+	 * start of the match, if one is found.
+	 * return length of match, or negative.
+	 */
+
+	int since_start, len;
+	struct match_state *st = rxl_prepare(rxl);
+
+	do {
+		mark_to_mark(endmark, m);
+		since_start = 0;
+		len = -1;
+		while (len == -1) {
+			wint_t wch = mark_next_pane(p, m);
+			if (wch == WEOF)
+				break;
+			since_start += 1;
+			len = rxl_advance(st, wch, 0, since_start == 1);
+		}
+		mark_to_mark(m, endmark);
+	} while(len < since_start &&
+		(!m2 || m2->seq < m->seq) &&
+		(mark_prev_pane(p, m) != WEOF));
+	rxl_free_state(st);
+	return len == since_start ? len : -1;
+}
+
 DEF_CMD(text_search)
 {
 	struct mark *m, *endmark = NULL;
@@ -60,7 +93,10 @@ DEF_CMD(text_search)
 	if (!endmark)
 		return -1;
 
-	since_start = search_forward(ci->focus, m, ci->mark2, rxl, endmark);
+	if (ci->extra)
+		since_start = search_backward(ci->focus, m, ci->mark2, rxl, endmark);
+	else
+		since_start = search_forward(ci->focus, m, ci->mark2, rxl, endmark);
 
 	if (since_start > 0)
 		mark_to_mark(m, endmark);
