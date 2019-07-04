28318a00fe4c0fe7a595c2136bedebc5f01ff8b2
NeilBrown <neil@brown.name>
2019-07-04 20:29:47 +1000

    viewer: use Window:bury for bury function.
    This should have been changed when I invented Window:bury
    
    Signed-off-by: NeilBrown <neil@brown.name>

--- lib-viewer.c
+++ lib-viewer.c
@@ -50,13 +50,7 @@ DEF_CMD(viewer_page_up)
 
 DEF_CMD(viewer_bury)
 {
-	struct pane *tile;
-	call("doc:revisit", ci->focus, -1);
-	tile = call_pane("ThisPane", ci->focus);
-	if (tile)
-		tile = pane_my_child(tile, ci->focus);
-	if (tile)
-		pane_close(tile);
+	call("Window:bury", ci->focus);
 	return 1;
 }
 
