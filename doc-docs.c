neilbrown/edlib

commit d3925302ba770c35fb3d9dceb7e1c6d7038a3921

Use Call:Notify:Doc... to ask a document to send a notification.
Though this isn't a important as with global notifications,
it is cleaner if the requet to send notificaitons is different
from the notifications sent.

Signed-off-by: NeilBrown <neil@brown.name>

doc-docs.c
@@ -403,7 +403,7 @@ DEF_CMD(docs_callback)
 		p = pane_register(p, 0, &docs_modified_handle.c, doc, NULL);
 		call("Request:Notify:doc:Replace", p);
 		/* And trigger Notify:doc:Replace handling immediately...*/
-		call("Notify:doc:Replace", p);
+		call("Call:Notify:doc:Replace", p);
 		/* Don't want to inherit position from some earlier instance,
 		 * always move to the start.
 		 */

core-doc.c
@@ -584,7 +584,7 @@ DEF_CMD(doc_request_notify)
 
 DEF_CMD(doc_notify)
 {
-	int ret = home_pane_notify(ci->home, ci->key, ci->focus, ci->num, ci->mark, ci->str,
+	int ret = home_pane_notify(ci->home, ci->key + 5, ci->focus, ci->num, ci->mark, ci->str,
 			      ci->num2, ci->mark2, ci->str2, ci->comm2);
 	/* Mustn't return 0, else will fall through to next doc */
 	return ret ?: Enotarget;
@@ -1095,7 +1095,7 @@ static void init_doc_cmds(void)
 
 	key_add_range(doc_handle_cmd, "doc:", "doc;", &doc_pass_on);
 	key_add_range(doc_handle_cmd, "Request:Notify:doc:", "Request:Notify:doc;", &doc_pass_on);
-	key_add_range(doc_handle_cmd, "Notify:doc:", "Notify:doc;", &doc_pass_on);
+	key_add_range(doc_handle_cmd, "Call:Notify:doc:", "Call:Notify:doc;", &doc_pass_on);
 
 	key_add(doc_handle_cmd, "Move-Char", &doc_char);
 	key_add(doc_handle_cmd, "Move-Word", &doc_word);
@@ -1139,7 +1139,7 @@ static void init_doc_cmds(void)
 
 	key_add_range(doc_default_cmd, "Request:Notify:doc:", "Request:Notify:doc;",
 		      &doc_request_notify);
-	key_add_range(doc_default_cmd, "Notify:doc:", "Notify:doc;",
+	key_add_range(doc_default_cmd, "Call:Notify:doc:", "Call:Notify:doc;",
 		      &doc_notify);
 	key_add_range(doc_default_cmd, "doc:set:", "doc:set;",
 		      &doc_set);

doc-text.c
@@ -305,7 +305,7 @@ DEF_CMD(text_load_file)
 		}
 	}
 	t->saved = t->undo;
-	call("Notify:doc:status-changed", ci->home);
+	call("Call:Notify:doc:status-changed", ci->home);
 	pane_notify("Notify:doc:Replace", t->doc.home);
 	if (fd != ci->num2)
 		close(fd);
@@ -496,7 +496,7 @@ DEF_CMD(text_save_file)
 	call("Message", ci->focus, 0, NULL, msg);
 	free(msg);
 	if (change_status)
-		call("Notify:doc:status-changed", d->home);
+		call("Call:Notify:doc:status-changed", d->home);
 	text_check_autosave(t);
 	if (ret == 0)
 		return 1;
@@ -1170,7 +1170,7 @@ DEF_CMD(text_reundo)
 	text_check_consistent(t);
 
 	if (status != (t->undo == t->saved))
-		call("Notify:doc:status-changed", t->doc.home);
+		call("Call:Notify:doc:status-changed", t->doc.home);
 	text_check_autosave(t);
 
 	/* Point probably moved, so */
@@ -1870,7 +1870,7 @@ DEF_CMD(text_replace)
 	}
 	text_check_autosave(t);
 	if (status_change)
-		call("Notify:doc:status-changed", d->home);
+		call("Call:Notify:doc:status-changed", d->home);
 	pane_notify("Notify:doc:Replace", t->doc.home, 0, pm, NULL,
 		    0, early);
 	if (!ci->mark2)
@@ -2003,7 +2003,7 @@ DEF_CMD(text_modified)
 	else
 		t->saved = t->undo;
 	text_check_autosave(t);
-	call("Notify:doc:status-changed", d->home);
+	call("Call:Notify:doc:status-changed", d->home);
 	return 1;
 }
 

lib-linecount.c
@@ -294,7 +294,7 @@ DEF_CMD(linecount_clip)
 DEF_CMD(count_lines)
 {
 	/* FIXME optimise this away most of the time */
-	if (call("Notify:doc:CountLines", ci->focus) == 0) {
+	if (call("Call:Notify:doc:CountLines", ci->focus) == 0) {
 		/* No counter in place, add one */
 		struct count_info *cli;
 		struct pane *p;
@@ -305,19 +305,19 @@ DEF_CMD(count_lines)
 		home_call(ci->focus, "Request:Notify:doc:Replace", p);
 		home_call(ci->focus, "Request:Notify:doc:CountLines", p);
 		home_call(ci->focus, "Request:Notify:doc:GotoLine", p);
-		call("Notify:doc:CountLines", ci->focus, 1, ci->mark);
+		call("Call:Notify:doc:CountLines", ci->focus, 1, ci->mark);
 	}
 	if (ci->mark) {
 		if (ci->str && strcmp(ci->str, "goto:line") == 0 &&
 		    ci->num != NO_NUMERIC) {
-			call("Notify:doc:GotoLine", ci->focus, ci->num, NULL, NULL,
+			call("Call:Notify:doc:GotoLine", ci->focus, ci->num, NULL, NULL,
 			     0, ci->mark);
 		}
-		call("Notify:doc:CountLines", ci->focus, 0, NULL, NULL,
+		call("Call:Notify:doc:CountLines", ci->focus, 0, NULL, NULL,
 		     0, ci->mark);
 	}
 	if (ci->mark2)
-		call("Notify:doc:CountLines", ci->focus, 0, NULL, NULL,
+		call("Call:Notify:doc:CountLines", ci->focus, 0, NULL, NULL,
 		      0, ci->mark2);
 	return 1;
 }

mode-emacs.c
@@ -1150,7 +1150,7 @@ DEF_CMD(emacs_search_highlight)
 		attr_set_int(&m->attrs, "render:search", ci->num);
 		call("Move-View-Pos", ci->focus, 0, m);
 	}
-	call("Notify:doc:Replace", ci->focus);
+	call("Call:Notify:doc:Replace", ci->focus);
 	pane_damaged(ci->home, DAMAGED_CONTENT|DAMAGED_VIEW);
 	return 1;
 }

python/lib-server.py
@@ -206,7 +206,7 @@ else:
 		ServerPane(new)
 
 	def server_done(key, focus, **a):
-		ret = focus.call("Notify:doc:done", "test")
+		ret = focus.call("Call:Notify:doc:done", "test")
 		if ret > 0:
 			# maybe save, then notify properly
 			fn = focus["filename"]
@@ -214,7 +214,7 @@ else:
 			if fn and mod == "yes":
 				focus.call("Message", "Please save first!")
 			else:
-				focus.call("Notify:doc:done")
+				focus.call("Call:Notify:doc:done")
 				# FIXME need something better than 'bury'
 				# If it was already visible, it should stay that way
 				focus.call("Window:bury")

python/render-present.py
@@ -709,20 +709,20 @@ class MarkdownPane(edlib.Pane):
         "handle:Display:refresh"
         # Refresh causes presentation page to recenter
         # page-down just moves down to start of next page.
-        focus.call("Notify:doc:Recentre", mark)
+        focus.call("Call:Notify:doc:Recentre", mark)
         return 0
 
     def handle_mvl(self, key, focus, mark, num, **a):
         "handle:Move-View-Large"
         if num >= 0 and mark:
             m2 = mark.dup()
-            if focus.call("Notify:doc:Recentre", m2, 2,
+            if focus.call("Call:Notify:doc:Recentre", m2, 2,
                           lambda key, **a: mark.to_mark(a['mark'])) > 0:
                 focus.damaged(edlib.DAMAGED_CURSOR)
                 return 1
         if num < 0 and mark:
             m2 = mark.dup()
-            if focus.call("Notify:doc:Recentre", m2, 3,
+            if focus.call("Call:Notify:doc:Recentre", m2, 3,
                           lambda key, **a: mark.to_mark(a['mark'])) > 0:
                 focus.damaged(edlib.DAMAGED_CURSOR)
                 return 1

commit cbf99699ecb22fb86f3eb3a9e3ab98ce1e6eb6e5

Fix crash when exit while doc-list open
A recent commit confused a notification call which
results in a freed mark being used and a crash.

Fixes: d3925302ba77 ("Use Call:Notify:Doc... to ask a document to send a notification.")

Signed-off-by: NeilBrown <neil@brown.name>

doc-docs.c
@@ -403,7 +403,7 @@ DEF_CMD(docs_callback)
 		p = pane_register(p, 0, &docs_modified_handle.c, doc, NULL);
 		call("Request:Notify:doc:Replace", p);
 		/* And trigger Notify:doc:Replace handling immediately...*/
-		call("Call:Notify:doc:Replace", p);
+		pane_call(p, "Notify:doc:Replace", p);
 		/* Don't want to inherit position from some earlier instance,
 		 * always move to the start.
 		 */