neilbrown/edlib

commit 2efec72da42d611b5a0ab03ef21c7ae7c2bdf591

Rename "render-line" to "doc:render-line"
This allows a document to handle rendering directly.
Also "render-line-prev" becomes "doc:render-line-prev".

Signed-off-by: NeilBrown <neil@brown.name>

python/render-present.py
@@ -81,12 +81,12 @@ class PresenterPane(edlib.Pane):
 
     def get_line_at(self, m):
         # call render-line at m
-        s = self.parent.call("render-line", m, -1, ret = 'str')
+        s = self.parent.call("doc:render-line", m, -1, ret = 'str')
         return s if s else ''
 
     def get_line_before(self, m):
         m2 = m.dup()
-        ret = self.parent.call("render-line-prev", m2, 1)
+        ret = self.parent.call("doc:render-line-prev", m2, 1)
         if ret <= 0:
             return None
         l = self.get_line_at(m2)
@@ -467,7 +467,7 @@ class PresenterPane(edlib.Pane):
         # Go to start of page
         if num == 0:
             # just make sure at start of line
-            return self.parent.call("render-line-prev", mark)
+            return self.parent.call("doc:render-line-prev", mark)
 
         start = self.find_pages(mark)
         if not start:

DOC/render-control
@@ -22,17 +22,17 @@ as text with attributes.
 lib-renderline
 --------------
 
-The two interfaces are "render-line" and "render-line-prev"
-"render-line" is the main worker, "render-line-prev" moves backwards
+The two interfaces are "doc:render-line" and "doc:render-line-prev"
+"doc:render-line" is the main worker, "doc:render-line-prev" moves backwards
 through a text buffer looking for a suitable place to start the line before 
 the given location.
 
-"render-line-prev" args are:
+"doc:render-line-prev" args are:
   mark: current location
   num: if 0, move to start of current line.  If 1, move back beyond start of
       current line, then to the start of the previous line
 
-"render-line"  args are:
+"doc:render-line"  args are:
   mark: start of the line to be rendered.  Line continues until first '\n'
         or until 'num2' chars have been examined with no '\n' found
         mark will be moved to the end of the range that was rendered (often
@@ -136,7 +136,8 @@ a cursor at the location of "point" in the document.
 It handles "Refresh" and related commands and calls "Draw:text" etc
 to update the display.
 
-render-lines will call "render-line" and "render-line-prev" to find out what should be drawn.
+render-lines will call "doc:render-line" and "doc:render-line-prev" to find
+out what should be drawn.
 They should return marked-up text providing attributes to guide drawing.
 The markup uses <> to enclose attributes - anything else is text.
 A '<' can be included in text by doubling it '<<'

core-doc.c
@@ -144,7 +144,7 @@ static wint_t doc_move_horiz(struct pane *p safe, struct mark *m safe,
 			     int *field)
 {
 	/* If subfields are present, move one step, else move one
-	 * char.  We we move a char and land in subfields, make
+	 * char.  If we move a char and land in subfields, make
 	 * sure rpos is correct.
 	 */
 	wint_t ret = '\n';

lib-renderline.c
@@ -407,8 +407,8 @@ void edlib_init(struct pane *ed safe)
 {
 	rl_map = key_alloc();
 
-	key_add(rl_map, "render-line", &render_line);
-	key_add(rl_map, "render-line-prev", &render_prev);
+	key_add(rl_map, "doc:render-line", &render_line);
+	key_add(rl_map, "doc:render-line-prev", &render_prev);
 	key_add(rl_map, "Clone", &rl_clone);
 	key_add(rl_map, "Close", &rl_close);
 	key_add(rl_map, "Notify:clip", &rl_clip);

render-complete.c
@@ -176,7 +176,7 @@ static int do_render_complete_prev(struct complete_data *cd safe, struct mark *m
 	cb.plen = strlen(cb.prefix);
 	cb.cmp = 0;
 	while (1) {
-		ret = call("render-line-prev", focus, n2, m2);
+		ret = call("doc:render-line-prev", focus, n2, m2);
 		if (ret <= 0 || n == 0)
 			/* Either hit start-of-file, or have what we need */
 			break;
@@ -188,7 +188,7 @@ static int do_render_complete_prev(struct complete_data *cd safe, struct mark *m
 		m3 = mark_dup(m2);
 		cb.keep = n2 == 1 && savestr;
 		cb.str = NULL;
-		if (call_comm("render-line", focus, &cb.c, NO_NUMERIC, m3)
+		if (call_comm("doc:render-line", focus, &cb.c, NO_NUMERIC, m3)
 		    != 1) {
 			mark_free(m3);
 			break;
@@ -335,7 +335,7 @@ DEF_CMD(complete_eol)
 	while (rpt > 1) {
 		struct call_return cr;
 		cr.c = eol_cb;
-		if (home_call(ci->home, "render-line",
+		if (home_call(ci->home, "doc:render-line",
 			      ci->focus, NO_NUMERIC, ci->mark, NULL,
 			      0, NULL, NULL, 0,0, &cr.c) <= 0)
 			rpt = 1;
@@ -481,7 +481,7 @@ DEF_CMD(complete_return)
 
 	cr.c = save_str;
 	cr.s = NULL;
-	home_call(ci->home, "render-line",
+	home_call(ci->home, "doc:render-line",
 		  ci->home, NO_NUMERIC, ci->mark, NULL, 0, NULL,
 		  NULL, 0,0, &cr.c);
 	if (!cr.s)
@@ -519,8 +519,8 @@ static void register_map(void)
 {
 	rc_map = key_alloc();
 
-	key_add(rc_map, "render-line", &render_complete_line);
-	key_add(rc_map, "render-line-prev", &render_complete_prev);
+	key_add(rc_map, "doc:render-line", &render_complete_line);
+	key_add(rc_map, "doc:render-line-prev", &render_complete_prev);
 	key_add(rc_map, "Close", &complete_close);
 	key_add(rc_map, "Clone", &complete_clone);
 

render-format.c
@@ -263,8 +263,8 @@ static void render_format_register_map(void)
 {
 	rf_map = key_alloc();
 
-	key_add(rf_map, "render-line", &render_line);
-	key_add(rf_map, "render-line-prev", &render_line_prev);
+	key_add(rf_map, "doc:render-line", &render_line);
+	key_add(rf_map, "doc:render-line-prev", &render_line_prev);
 	key_add(rf_map, "Close", &format_close);
 	key_add(rf_map, "Clone", &format_clone);
 	key_add(rf_map, "doc:get-attr", &format_get_attr);

render-hex.c
@@ -203,8 +203,8 @@ static void render_hex_register_map(void)
 	key_add(he_map, "Move-EOL", &render_hex_eol);
 	key_add(he_map, "doc:step", &hex_step);
 
-	key_add(he_map, "render-line-prev", &render_line_prev);
-	key_add(he_map, "render-line", &render_line);
+	key_add(he_map, "doc:render-line-prev", &render_line_prev);
+	key_add(he_map, "doc:render-line", &render_line);
 
 	key_add(he_map, "Close", &render_hex_close);
 	key_add(he_map, "Clone", &render_hex_clone);

render-lines.c
@@ -4,10 +4,10 @@
  *
  * Rendering for any document which presents as a sequence of lines.
  * The underlying document must return lines of text in response to
- * the "render-line" command.
+ * the "doc:render-line" command.
  * This takes a mark and moves it to the end of the rendered line
  * so that another call will produce another line.
- * "render-line" must always return a full line including '\n' unless the result
+ * "doc:render-line" must always return a full line including '\n' unless the result
  * would be bigger than the 'max' passed in ->num2.  In that case it can stop
  * after 'max' and before a '\n'.
  * If ->num is >= 0, then rendering must only preceed for that many bytes
@@ -18,7 +18,7 @@
  * For the standard 'render the whole line' functionality, ->num should
  * be NO_NUMERIC
  *
- * The document must also provide "render-line-prev" which moves mark to a
+ * The document must also provide "doc:render-line-prev" which moves mark to a
  * start-of-line.  If num is 0, then don't skip over any newlines.
  * If it is '1', then skip one newline.
  *
@@ -46,13 +46,13 @@
  * In the worst case of there being no newlines in the document, there will be precisely
  * two marks: one contains a partial line and one that marks the end of that line.
  * When point moves outside that range a new start will be chosen before point
- * using "render-line-prev' and the old start is discarded.
+ * using "doc:render-line-prev" and the old start is discarded.
  *
  * To render the pane we:
  * 1/ call 'render-line-prev' on a mark at the point and look for that mark
  *    in the view.
  * 2/ If the mark matches and has a string, we have a starting point, else we
- *    call "render-line" and store the result, thus producing a starting point.
+ *    call "doc:render-line" and store the result, thus producing a starting point.
  *    We determine how many display lines are needed to display this text-line and
  *    set 'y' accordingly.
  *    At this point we have two marks: start and end, with known text of known
@@ -828,7 +828,7 @@ static struct mark *call_render_line_prev(struct pane *p safe,
 	int ret;
 	struct mark *m2;
 
-	ret = call("render-line-prev", p, n, m);
+	ret = call("doc:render-line-prev", p, n, m);
 	if (ret <= 0) {
 		mark_free(m);
 		return NULL;
@@ -864,7 +864,7 @@ static struct mark *call_render_line(struct pane *p safe, struct mark *start saf
 	 * 'used' can be negative if the mark is before the start
 	 * of the pane
 	 */
-	s = call_ret(str, "render-line", p, NO_NUMERIC, m);
+	s = call_ret(str, "doc:render-line", p, NO_NUMERIC, m);
 
 	if (s) {
 		if (start->mdata)
@@ -903,7 +903,7 @@ static struct mark *call_render_line_offset(struct pane *p safe,
 	struct mark *m;
 
 	m = mark_dup_view(start);
-	if (call_comm("render-line", p, &no_save, offset, m) <= 0) {
+	if (call_comm("doc:render-line", p, &no_save, offset, m) <= 0) {
 		mark_free(m);
 		return NULL;
 	}
@@ -927,7 +927,7 @@ static int call_render_line_to_point(struct pane *p safe, struct mark *pm safe,
 	int len;
 	struct mark *m = mark_dup_view(start);
 
-	len = call_comm("render-line", p, &get_len, -1, m, NULL, 0, pm);
+	len = call_comm("doc:render-line", p, &get_len, -1, m, NULL, 0, pm);
 	mark_free(m);
 	if (len <= 0)
 		return 0;

commit d7ce3998ec1e515ba4d99045a627f04c1278680b

render-present: fix handling of render-line
When I renamed render-line to doc:render-line,
I missed some bits in render-present.

Fixes: 2efec72da42d ("Rename "render-line" to "doc:render-line"")
Signed-off-by: NeilBrown <neil@brown.name>

python/render-present.py
@@ -463,7 +463,7 @@ class PresenterPane(edlib.Pane):
         return 0
 
     def handle_render_prev(self, key, focus, mark, mark2, num, comm2, **a):
-        "handle:render-line-prev"
+        "handle:doc:render-line-prev"
         # Go to start of page
         if num == 0:
             # just make sure at start of line
@@ -481,7 +481,7 @@ class PresenterPane(edlib.Pane):
         return 1
 
     def handle_render_line(self, key, focus, mark, mark2, num, comm2, **a):
-        "handle:render-line"
+        "handle:doc:render-line"
         page = self.find_pages(mark)
         if not page:
             # No pages at all