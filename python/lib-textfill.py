facd08263bfc8641ae17e48f8b5d7ea2dcbc284a
NeilBrown <neil@brown.name>
2019-10-16 19:55:59 +1100

    textfill: change rules for assigning prefix.
    If there is a second line, it determines the prefix for the rest.
    The first line only determines what characters can be in the prefix.
    
    If there is no second line, then the prefix is whitespace of the
    same length as the first-line prefix.
    
    Signed-off-by: NeilBrown <neil@brown.name>

--- python/lib-textfill.py
+++ python/lib-textfill.py
@@ -10,14 +10,34 @@
 #   fit a given width.
 #
 # This first version only has limited fill-paragragh.
-# Two marks are provided and the text between them is re-filled.
-# Any text between the first mark and the start of that line is
-# optionally a prefix.  If any other line starts with the same string,
-# it is stripped off, to be re-added (to all lines) after filling.
-# If the prefix doesn't appear, or is all spaces, then the shortest
-# existing space-prefix on para-lines is used as a prefix.
+# Two marks are provided and the text between them is re-filled.  Any
+# text between the first mark and the start of that line is used to supply
+# characters for a prefix.  Any characters in that prefix, and any white
+# space, are striped from the start of every other line.  If there is a
+# second line, the text stripped from there is prefixed to all lines of
+# the reformatted paragraph.  if there is no second line, then prefix
+# provides is the prefix of the first line with all non-tabs replaced with
+# spaces.
 
 import re
+
+def span(line, chars):
+    s = ''
+    for c in line:
+        if c in chars:
+            s += c
+        else:
+            break
+    return s
+
+def textwidth(line):
+    w = 0
+    for c in line:
+        if c == '\t':
+            w = w | 7
+        w += 1
+    return w
+
 
 class FillMode(edlib.Pane):
     def __init__(self, focus):
@@ -69,42 +89,41 @@ class FillMode(edlib.Pane):
 
         m = mark.dup()
         focus.call("Move-EOL", -1, m)
-        prefix = focus.call("doc:get-str", m, mark, ret='str')
+        prefix0 = focus.call("doc:get-str", m, mark, ret='str')
         para = focus.call("doc:get-str", mark, mark2, ret='str')
         lines = para.splitlines()
         if len(lines) == 0:
             return 1
 
-        plen = len(prefix)
-        use_prefix = False
-        if plen:
-            first = True
-            for l in lines:
-                if prefix == l[:len(prefix)]:
-                    use_prefix = True
-                p = len(l) - len(l.lstrip())
-                if p < plen and not first:
-                    plen = p
-                first = False
-        if prefix.lstrip() == '':
-            use_prefix = False
+        tostrip = prefix0 + ' \t'
+        if len(lines) == 1:
+            # only one line, so prefix is all spaces but based on first line
+            prefix = ""
+            for c in prefix0:
+                if c == '\t':
+                    prefix += c
+                else:
+                    prefix += ' '
         else:
-            plen = len(prefix)
+            prefix = span(lines[1], tostrip)
+
+        plen = textwidth(prefix)
+        plen0 = textwidth(prefix0)
+
         words = []
         for l in lines:
-            if prefix == l[:len(prefix)]:
-                l = l[len(prefix):]
+            p = span(l, tostrip)
+            l = l[len(p):]
             words.extend(re.split(r'\s+', l.strip()))
+
         if len(words) < 2:
             return
 
         # we have the words, time to assemble the lines
         # first line never gets prefix
         newpara = ""
-        ln= ""
-        lln = len(prefix)
-        if not use_prefix:
-            prefix = ' '*plen
+        ln = ""
+        lln = plen0
 
         pfx = ''
         for w in words:
@@ -115,12 +134,12 @@ class FillMode(edlib.Pane):
             if ln and lln + spaces + len(w) > width:
                 # time for a line break
                 newpara += pfx + ln + '\n'
-                ln = ''
-                lln = 0
+                ln = ""
+                lln = plen
                 pfx = prefix
             if ln:
                 ln += ' ' * spaces
-                lln += 1
+                lln += spaces
             ln += w
             lln += len(w)
         newpara += pfx + ln
