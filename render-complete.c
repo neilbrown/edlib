ca268e4ff2b774942bf0e1e7ac852048ffeb4e53
NeilBrown <neil@brown.name>
2020-02-21 18:58:57 +1100

    render-complete: fix completion of doc names.
    This code seemed to assume that doc name are sorted, but they aren't.  So it broke.
    Particularly if the doc names were, for example
    
     doc-text.c
     core-doc.c
     doc-dir.c
     doc-doc.s
    
    in reverse order (because they are examined from last to first),
    then when it saw doc-text.c it would set at_start to 1,
    then core-doc.c would cause at_start to be set to 2.
    This caused all later doc* to be ignored for the purpose of
    calculating "common".
    
    Signed-off-by: NeilBrown <neil@brown.name>

--- render-complete.c
+++ render-complete.c
@@ -411,6 +411,8 @@ DEF_CMD(complete_set_prefix)
 	while (do_render_complete_prev(cd, m, p->parent, 1, &c) > 0 && c) {
 		int l;
 		char *match = c;
+		int start_matches;
+
 		if (!cd->prefix_only)
 			match = strstr(match, cd->prefix);
 		if (!match)
@@ -418,21 +420,23 @@ DEF_CMD(complete_set_prefix)
 		l = strlen(match);
 		if (l && match[l-1] == '\n')
 			l -= 1;
-		if (!cd->prefix_only && strncmp(c, cd->prefix, strlen(cd->prefix)) == 0) {
+		start_matches = cd->prefix_only ||
+			strncmp(c, cd->prefix, strlen(cd->prefix)) == 0;
+		if (!cd->prefix_only && start_matches) {
 			if (m2)
 				mark_free(m2);
 			m2 = mark_dup(m);
 			if (!at_start) {
-				/* If there are matches at the start, the common prefix
-				 * calculation only noticed them
+				/* If there are matches at the start, the
+				 * common prefix calculation only noticed them
 				 */
 				at_start = 1;
 				free(common);
 				common = NULL;
 			}
-		} else if (at_start)
-			at_start = 2;
-		if (at_start != 2) {
+		}
+
+		if (!at_start || start_matches) {
 			if (common == NULL)
 				common = strndup(match, l);
 			else
