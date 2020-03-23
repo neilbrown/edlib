neilbrown/edlib

commit 8a74a109d282c921ac0af846c7b0ef2b725a2032

make/shell: decode utf-8 one line at a time.
We currently read bytes from the pipe connected to the subprocess in
chunks of up to 1024 bytes, and decode these via utf-8.  It is quite
possible that such a block ends with part of a multi-byte utf-8 code
pointer, so decoding can fail.

Change to instead break the incoming stream at each newline (which
cannot be in the middle of a utf-8 codepoint) and decode the
sets-of-lines.

Signed-off-by: NeilBrown <neil@brown.name>

python/lib-make.py
@@ -34,6 +34,7 @@ class MakePane(edlib.Pane):
         self.timer_set = False
         self.note_ok = False
         self.first_match = True
+        self.line = b''
         self.call("editor:request:make:match-docs")
 
     def run(self, cmd, cwd):
@@ -67,12 +68,20 @@ class MakePane(edlib.Pane):
         if r is None or len(r) == 0:
             (out, err) = self.pipe.communicate()
             self.pipe = None
-            self.call("doc:replace", out.decode("utf-8"))
+            l = self.line + out
+            self.line = b''
+            self.call("doc:replace", l.decode("utf-8"))
             self.call("doc:replace", "\nProcess Finished\n");
             self.call("doc:set:doc-status", "Complete")
             self.call("doc:notify:doc:status-changed")
+            self.do_parse()
             return edlib.Efalse
-        self.call("doc:replace", r.decode("utf-8"));
+        l = self.line + r
+        i = l.rfind(b'\n')
+        if i >= 0:
+            self.call("doc:replace", r[:i+1].decode("utf-8"));
+            l = l[i+1:]
+        self.line = l
         self.do_parse()
         return 1
 

python/lib-shellcmd.py
@@ -8,6 +8,7 @@ import subprocess, os, fcntl
 class ShellPane(edlib.Pane):
     def __init__(self, focus):
         edlib.Pane.__init__(self, focus)
+        self.line = b''
 
     def run(self, cmd, cwd):
         FNULL = open(os.devnull, 'r')
@@ -38,10 +39,17 @@ class ShellPane(edlib.Pane):
         if r is None or len(r) == 0:
             (out,err) = self.pipe.communicate()
             self.pipe = None
-            self.call("doc:replace", out.decode("utf-8"))
+            l = self.line + out
+            self.line = b''
+            self.call("doc:replace", l.decode("utf-8"))
             self.call("doc:replace", "\nProcess Finished\n");
             return edlib.Efalse
-        self.call("doc:replace", r.decode("utf-8"));
+        l = self.line + r
+        i = l.rfind(b'\n')
+        if i >= 0:
+            self.call("doc:replace", l[:i+1].decode("utf-8"));
+            l = l[i+1:]
+        self.line = l
         return 1
 
     def handle_close(self, key, **a):

commit fade6a44452b15267cb8024a071b52b82109d940

make: fix typo when adding text to document.
I need to add from the 'l' string, not the 'r' string.

Fixes: 8a74a109d282 ("make/shell: decode utf-8 one line at a time.")
Signed-off-by: NeilBrown <neil@brown.name>

python/lib-make.py
@@ -86,7 +86,7 @@ class MakePane(edlib.Pane):
         l = self.line + r
         i = l.rfind(b'\n')
         if i >= 0:
-            self.call("doc:replace", r[:i+1].decode("utf-8"));
+            self.call("doc:replace", l[:i+1].decode("utf-8"));
             l = l[i+1:]
         self.line = l
         self.do_parse()