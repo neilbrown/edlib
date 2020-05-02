neilbrown/edlib

commit ef8dad034528d7a6e4f61fe61a097d016de12274

Remove 'rpos' functionality.
This was a design mistake and is going away.

The doc-rendering pane can do something similar, and in
some ways much better.
If something more similar is needed, the 'render pos' should
be stored in a display pane associated with just the 'point',
not with every mark.  It is then used to display the cursor.
The same pane would capture left/right movement and update
the curspor pos.

Note that this breaks some aspects of email handling.
That needs a lot of work anyway...

Signed-off-by: NeilBrown <neil@brown.name>

doc-email.c
@@ -60,13 +60,8 @@ static int cond_append(struct buf *b safe, char *txt safe, char *tag safe,
 	buf_concat(b, tagf);
 	buf_concat(b, tag);
 	buf_concat(b, ">[");
-	if (pm && pm->rpos == m->rpos) {
-		buf_concat_len(b, txt, strlen(txt)/2);
-		return 0;
-	}
 	buf_concat(b, txt);
 	buf_concat(b, "]</>");
-	m->rpos += 1;
 	return 1;
 }
 
@@ -101,9 +96,6 @@ DEF_CMD(email_spacer)
 	if (!attr)
 		attr = "hide";
 
-	m->rpos = 0;
-	if (pm && (pm->rpos == NO_RPOS || pm->rpos == NEVER_RPOS))
-		pm->rpos = 0;
 	buf_init(&b);
 	buf_concat(&b, "<fg:red>");
 
@@ -122,10 +114,8 @@ DEF_CMD(email_spacer)
 	if (ok) {
 		if ((o < 0 || o == NO_NUMERIC)) {
 			buf_concat(&b, "</>\n");
-			m->rpos = 0;
 			mark_next_pane(ci->focus, m);
-		} else
-			m->rpos -= 1;
+		}
 	}
 
 	ret = comm_call(ci->comm2, "callback:render", ci->focus, 0, NULL,
@@ -139,7 +129,7 @@ DEF_CMD(email_select)
 	/* If mark is on a button, press it... */
 	struct mark *m = ci->mark;
 	char *a;
-	int r;
+	int r = 0;
 
 	if (!m)
 		return Enoarg;
@@ -149,7 +139,6 @@ DEF_CMD(email_select)
 	a = pane_mark_attr(ci->home, m, "multipart-prev:email:actions");
 	if (!a)
 		a = "hide";
-	r = m->rpos;
 	while (r > 0 && a) {
 		a = strchr(a, ':');
 		if (a)

render-format.c
@@ -38,10 +38,7 @@ static char *do_format(struct rf_data *rf safe, struct pane *focus safe,
 	if (!body)
 		body = "%+name";
 	n = body;
-	m->rpos = field;
-	if (pm && (pm->rpos == NO_RPOS|| pm->rpos == NEVER_RPOS))
-		pm->rpos = rf->home_field;
-	if (pm && pm->rpos == m->rpos)
+	if (pm)
 		goto endwhile;
 	if (len >= 0 && ret.len >= len)
 		goto endwhile;
@@ -68,11 +65,10 @@ static char *do_format(struct rf_data *rf safe, struct pane *focus safe,
 		}
 		if (n[1] == '+' || n[1] == '.')
 			field += 1;
-		m->rpos = field;
 
 		if (len >= 0 && ret.len >= len)
 			break;
-		if (pm && pm->rpos == m->rpos)
+		if (pm)
 			break;
 		n += 1;
 		if (*n == '+') {
@@ -139,10 +135,7 @@ endwhile:
 	if (!*n) {
 		rf->fields = field+1;
 		rf->home_field = home;
-		m->rpos = field;
-		if (pm && pm->rpos == m->rpos)
-			;
-		else if (len >= 0)
+		if (len >= 0)
 			;
 		else
 			buf_append(&ret, '\n');

core-doc.c
@@ -80,74 +80,6 @@ struct pane *safe doc_register(struct pane *parent, int z,
 	return doc->home;
 }
 
-static void parse_sub_pos(char *attr, int *home safe, int *max safe)
-{
-	char *c;
-	if (!attr) {
-		*home = 0;
-		*max = 1;
-		return;
-	}
-	c = strchr(attr, ':');
-	if (c) {
-		*home = atoi(attr);
-		*max = atoi(c+1);
-	} else {
-		*home = 0;
-		*max = atoi(attr);
-	}
-	if (*max <= 0)
-		*max = 1;
-	if (*home < 0 || *home > *max)
-		*home = 0;
-}
-
-static wint_t doc_move_horiz(struct pane *p safe, struct mark *m safe,
-			     int forward,
-			     int *field)
-{
-	/* If subfields are present, move one step, else move one
-	 * char.  If we move a char and land in subfields, make
-	 * sure rpos is correct.
-	 */
-	wint_t ret = '\n';
-
-	if (field)
-		*field = 0;
-	if (m->rpos == NEVER_RPOS)
-		return mark_step_pane(p, m, forward, 1);
-	if (m->rpos != NO_RPOS) {
-		char *a = pane_mark_attr(p, m, "renderline:fields");
-		int home, cnt;
-		parse_sub_pos(a, &home, &cnt);
-		if (forward && m->rpos+1 < cnt)
-			m->rpos += 1;
-		else if (!forward && m->rpos > 0)
-			m->rpos -= 1;
-		else
-			m->rpos = NO_RPOS;
-	}
-	if (m->rpos == NO_RPOS) {
-		ret = mark_step_pane(p, m, forward, 1);
-		if (is_eol(forward ? mark_step_pane(p, m, forward, 0) : ret)) {
-			char *a = pane_mark_attr(p, m, "renderline:fields");
-			int home, cnt;
-			parse_sub_pos(a, &home, &cnt);
-			if (a) {
-				ret = '\n';
-				if (field)
-					*field = 1;
-				if (forward)
-					m->rpos = 0;
-				else
-					m->rpos = cnt-1;
-			}
-		}
-	} else if (field)
-		*field = 1;
-	return ret;
-}
-
 /* For these 'default commands', home->data is struct doc */
 DEF_CMD(doc_char)
 {
@@ -160,12 +92,12 @@ DEF_CMD(doc_char)
 		m = dd->point;
 
 	while (rpt > 0) {
-		if (doc_move_horiz(f, m, 1, NULL) == WEOF)
+		if (mark_step_pane(f, m, 1, 1) == WEOF)
 			break;
 		rpt -= 1;
 	}
 	while (rpt < 0) {
-		if (doc_move_horiz(f, m, 0, NULL) == WEOF)
+		if (mark_step_pane(f, m, 0, 1) == WEOF)
 			break;
 		rpt += 1;
 	}
@@ -194,21 +126,18 @@ DEF_CMD(doc_word)
 	 */
 	dir = rpt > 0 ? 1 : 0;
 	while (rpt != 0) {
-		int field = 0;
 		wint_t wi;
 
-		while (!field &&
-		       iswspace(mark_step_pane(f, m, dir, 0)))
-			doc_move_horiz(f, m, dir, &field);
+		while (iswspace(mark_step_pane(f, m, dir, 0)))
+			mark_step_pane(f, m, dir, 1);
 
-		while (!field &&
-		       (wi=mark_step_pane(f, m, dir, 0)) != WEOF &&
+		while ((wi=mark_step_pane(f, m, dir, 0)) != WEOF &&
 		       !iswspace(wi) && !iswalnum(wi))
-			doc_move_horiz(f, m, dir, &field);
+			mark_step_pane(f, m, dir, 1);
 
-		if (m->rpos < NO_RPOS || iswalnum(mark_step_pane(f, m, dir, 0))) {
-			while (!field && iswalnum(mark_step_pane(f, m, dir, 0)))
-				doc_move_horiz(f, m, dir, &field);
+		if (iswalnum(mark_step_pane(f, m, dir, 0))) {
+			while (iswalnum(mark_step_pane(f, m, dir, 0)))
+				mark_step_pane(f, m, dir, 1);
 		}
 
 		rpt -= dir * 2 - 1;
@@ -318,27 +247,23 @@ DEF_CMD(doc_WORD)
 	/* We skip spaces, then non-spaces */
 	while (rpt > 0) {
 		wint_t wi;
-		int field = 0;
-		while (!field &&
-		       iswspace(doc_following_pane(f, m)))
-			doc_move_horiz(f, m, 1, &field);
 
-		while (!field &&
-		       (wi=doc_following_pane(f, m)) != WEOF &&
+		while (iswspace(doc_following_pane(f, m)))
+			mark_step_pane(f, m, 1, 1);
+
+		while ((wi=doc_following_pane(f, m)) != WEOF &&
 		       !iswspace(wi))
-			doc_move_horiz(f, m, 1, &field);
+			mark_step_pane(f, m, 1, 1);
 		rpt -= 1;
 	}
 	while (rpt < 0) {
 		wint_t wi;
-		int  field = 0;
-		while (!field &&
-		       iswspace(doc_prior_pane(f, m)))
-			doc_move_horiz(f, m, 0, &field);
-		while (!field &&
-		       (wi=doc_prior_pane(f, m)) != WEOF &&
+
+		while (iswspace(doc_prior_pane(f, m)))
+			mark_step_pane(f, m, 0, 1);
+		while ((wi=doc_prior_pane(f, m)) != WEOF &&
 		       !iswspace(wi))
-			doc_move_horiz(f, m, 0, &field);
+			mark_step_pane(f, m, 0, 1);
 		rpt += 1;
 	}
 

core-mark.c
@@ -151,7 +151,6 @@ static void dup_mark(struct mark *orig safe, struct mark *new safe)
 	INIT_TLIST_HEAD(&new->view, GRP_MARK);
 	new->attrs= NULL;
 	assign_seq(new, orig->seq);
-	new->rpos = orig->rpos;
 	mark_ref_copy(new, orig);
 }
 
@@ -168,7 +167,6 @@ struct mark *do_mark_at_point(struct mark *pt safe, int view)
 
 	dup_mark(pt, ret);
 	ret->viewnum = view;
-	ret->rpos = pt->rpos;
 	if (view >= 0)
 		tlist_add(&ret->view, GRP_MARK, &lnk->lists[view]);
 	else
@@ -268,8 +266,6 @@ void mark_to_end(struct doc *d safe, struct mark *m safe, int end)
 	int seq = 0;
 	struct point_links *lnk;
 
-	if (m->rpos != NEVER_RPOS)
-		m->rpos = NO_RPOS;
 	hlist_del(&m->all);
 	if (end) {
 		if (hlist_empty(&d->marks))
@@ -317,7 +313,6 @@ void mark_to_end(struct doc *d safe, struct mark *m safe, int end)
 
 void mark_reset(struct doc *d safe, struct mark *m safe, int end)
 {
-	m->rpos = NEVER_RPOS;
 	pane_call(d->home, "doc:set-ref", d->home, !end, m);
 }
 
@@ -395,8 +390,6 @@ static wint_t mark_step(struct doc *d safe, struct mark *m safe, int forward, in
 {
 	int ret;
 
-	if (move && m->rpos != NEVER_RPOS)
-		m->rpos = NO_RPOS;
 	ret = pane_call(d->home, "doc:step", d->home, forward, m, NULL, move);
 
 	if (ret <= 0)
@@ -410,8 +403,7 @@ static wint_t mark_step(struct doc *d safe, struct mark *m safe, int forward, in
 wint_t mark_step_pane(struct pane *p safe, struct mark *m safe, int forward, int move)
 {
 	int ret;
-	if (move && m->rpos != NEVER_RPOS)
-		m->rpos = NO_RPOS;
+
 	ret = call("doc:step", p, forward, m, NULL, move);
 
 	if (ret <= 0)
@@ -682,7 +674,6 @@ void mark_to_mark(struct mark *m safe, struct mark *target safe)
 		return;
 	mark_to_mark_noref(m, target);
 	mark_ref_copy(m, target);
-	m->rpos = target->rpos;
 }
 
 /* A 'vmark' is a mark in a particular view.  We can walk around those

core.h
@@ -179,11 +179,6 @@ struct mark {
 	struct attrset		*attrs;
 	int			seq;
 	short			viewnum;
-	unsigned short		rpos;	/* Used by renderer to identify
-					 * positions within a document object
-					 * (which displays as more than one
-					 * char).
-					 */
 	unsigned short		refs;	/* create of mark can use this for
 					 * e.g. refcounting
 					 */
@@ -191,9 +186,6 @@ struct mark {
 	void			*mtype;	/* can be used to validate type of mdata */
 	void			(*refcnt)(struct mark *m safe, int cnt);
 };
-
-#define NEVER_RPOS (USHRT_MAX)
-#define NO_RPOS (USHRT_MAX-1)
 
 /* A point uses this for the mdata */
 struct point_links {

lang-python.c
@@ -22,7 +22,6 @@
  *  edlib.mark  - these reference locations in a document.  The document is
  *                not directly accessible, it can only be accessed through
  *                a pane (which may translate events and results).
- *                get/set operations for rpos.
  *                get/set for viewnum (cannot be set)
  *                iterator for both 'all' and 'view' lists.
  *                no methods yet.
@@ -1430,20 +1429,18 @@ static PyTypeObject DocType = {
     .tp_new = (newfunc)Doc_new,/* tp_new */
 };
 
-static PyObject *mark_getrpos(Mark *m safe, void *x)
+static PyObject *mark_getoffset(Mark *m safe, void *x)
 {
 	if (m->mark == NULL) {
 		PyErr_SetString(PyExc_TypeError, "Mark is NULL");
 		return NULL;
 	}
-	if (x == NULL)
-		return PyInt_FromLong(m->mark->rpos);
 	if (m->local)
 		return PyInt_FromLong(m->mark->ref.o);
 	return PyInt_FromLong(0);
 }
 
-static int mark_setrpos(Mark *m safe, PyObject *v safe, void *x)
+static int mark_setoffset(Mark *m safe, PyObject *v safe, void *x)
 {
 	long val;
 
@@ -1454,9 +1451,7 @@ static int mark_setrpos(Mark *m safe, PyObject *v safe, void *x)
 	val = PyInt_AsLong(v);
 	if (val == -1 && PyErr_Occurred())
 		return -1;
-	if (x == NULL)
-		m->mark->rpos = val;
-	else if (m->local)
+	if (m->local)
 		m->mark->ref.o = val;
 	else {
 		PyErr_SetString(PyExc_TypeError, "Setting offset on non-local mark");
@@ -1594,11 +1589,8 @@ static PyGetSetDef mark_getseters[] = {
      (getter)mark_getpos, (setter)mark_setpos,
      "Position ref", NULL},
     {"offset",
-     (getter)mark_getrpos, (setter)mark_setrpos,
-     "Position offset", (void*)1},
-    {"rpos",
-     (getter)mark_getrpos, (setter)mark_setrpos,
-     "Rendering Position",  NULL},
+     (getter)mark_getoffset, (setter)mark_setoffset,
+     "Position offset", NULL},
     {"viewnum",
      (getter)mark_getview, (setter)mark_nosetview,
      "Index for view list", NULL},
@@ -2282,8 +2274,6 @@ void edlib_init(struct pane *ed safe)
 	PyModule_AddIntMacro(m, DAMAGED_CURSOR);
 	PyModule_AddIntMacro(m, DAMAGED_POSTORDER);
 	PyModule_AddIntMacro(m, DAMAGED_CLOSED);
-	PyModule_AddIntMacro(m, NO_RPOS);
-	PyModule_AddIntMacro(m, NEVER_RPOS);
 	PyModule_AddIntMacro(m, Efallthrough);
 	PyModule_AddIntMacro(m, Enoarg);
 	PyModule_AddIntMacro(m, Einval);

commit c33c878b3ee95829fb2a540dac3525b5d2138d15

Remove markup:fields support.
This should have been removed long ago.

With this code, the 'field' tracking stuff in render-format can go, and
the line-formats don't need '%.' or '%+' as it doesn't mean anything.
Maybe it will come back later, but it should come back with a fresh
design.

Fixes: ef8dad034528 ("Remove 'rpos' functionality.")
Signed-off-by: NeilBrown <neil@brown.name>

doc-dir.c
@@ -700,7 +700,7 @@ DEF_CMD(dir_get_attr)
 	else if (strcmp(attr, "doc-type") == 0)
 		val = "dir";
 	else if (strcmp(attr, "line-format") == 0)
-		val = " <fg:red>%.perms</> %.mdate:13 %.user:10 %.group:10%.hsize:-6  <fg:blue>%+name%.suffix</>%arrow<fg:green-30>%target</>";
+		val = " <fg:red>%perms</> %mdate:13 %user:10 %group:10%hsize:-6  <fg:blue>%name%suffix</>%arrow<fg:green-30>%target</>";
 	else if (strcmp(attr, "filename") == 0)
 		val = dr->fname;
 	else
@@ -866,7 +866,7 @@ DEF_CMD(dir_attach)
 		if (p)
 			p = call_ret(pane, "attach-viewer", p);
 		if (p) {
-			attr_set_str(&p->attrs, "line-format", "%+name%suffix");
+			attr_set_str(&p->attrs, "line-format", "%name%suffix");
 			attr_set_str(&p->attrs, "heading", "");
 			attr_set_str(&p->attrs, "done-key", "Replace");
 			p = call_ret(pane, "attach-render-complete", p);

doc-docs.c
@@ -633,7 +633,7 @@ DEF_CMD(docs_get_attr)
 	else if (strcmp(attr, "heading") == 0)
 		val = "<bold,underline> Mod Document             File</>";
 	else if (strcmp(attr, "line-format") == 0)
-		val = " %doc-modified:3 %+doc-name:20 %.filename";
+		val = " %doc-modified:3 %doc-name:20 %filename";
 	else if (strcmp(attr, "render-default") == 0)
 		val = "format";
 	else if (strcmp(attr, "view-default") == 0)
@@ -847,7 +847,7 @@ DEF_CMD(docs_attach)
 		if (p)
 			p = call_ret(pane, "attach-render-format", p);
 		if (p) {
-			attr_set_str(&p->attrs, "line-format", "%+doc-name");
+			attr_set_str(&p->attrs, "line-format", "%doc-name");
 			attr_set_str(&p->attrs, "heading", "");
 			attr_set_str(&p->attrs, "done-key", "Replace");
 			p = call_ret(pane, "attach-render-complete", p);
@@ -866,9 +866,6 @@ DEF_CMD(docs_attach)
 		if (p)
 			p = call_ret(pane, "attach-viewer", p);
 		if (p) {
-			//attr_set_str(&p->attrs, "line-format", "%+doc-name");
-			//attr_set_str(&p->attrs, "heading", "");
-			//attr_set_str(&p->attrs, "done-key", "Replace");
 			p = pane_register(p, 0, &docs_modified_handle.c, docs);
 
 			call("doc:request:doc:replaced", p);

doc-email.c
@@ -156,33 +156,6 @@ DEF_CMD(email_select)
 	return 1;
 }
 
-DEF_CMD(email_get_attr)
-{
-	/* The "markup:fields" attribute needs to be synthesized
-	 * from the per-part email:actions attribute
-	 */
-	char *a;
-	int fields;
-	char ret[12];
-	if (!ci->str || strcmp(ci->str, "markup:fields") != 0)
-		return Efallthrough;
-	if (!ci->mark)
-		return Efallthrough;
-
-	a = pane_mark_attr(ci->home->parent, ci->mark,
-			   "multipart-prev:email:actions");
-	if (!a)
-		return 1;
-	fields = 0;
-	while (a && *a) {
-		a = strchr(a, ':');
-		if (a)
-			a += 1;
-		fields += 1;
-	}
-	snprintf(ret, sizeof(ret), "%d", fields);
-	return comm_call(ci->comm2, "callback", ci->focus, 0, ci->mark, ret);
-}
 static struct map *email_map safe;
 static struct map *email_view_map safe;
 
@@ -816,7 +789,6 @@ static void email_init_map(void)
 	key_add(email_map, "Free", &edlib_do_free);
 	key_add(email_map, "doc:email:render-spacer", &email_spacer);
 	key_add(email_map, "doc:email:select", &email_select);
-	key_add(email_map, "doc:get-attr", &email_get_attr);
 
 	email_view_map = key_alloc();
 	key_add(email_view_map, "Free", &email_view_free);

python/module-notmuch.py
@@ -815,7 +815,7 @@ class notmuch_main_view(edlib.Pane):
         edlib.Pane.__init__(self, focus)
         self['render-wrap'] = 'no'
         self['background'] = 'color:#A0FFFF'
-        self['line-format'] = '<%fmt>%count %+name</>'
+        self['line-format'] = '<%fmt>%count %name</>'
         self.call("notmuch:set_list_pane")
         self.call("doc:request:doc:replaced")
         self.selected = None
@@ -871,7 +871,7 @@ class notmuch_list(edlib.Doc):
         self.messageids = {}
         self.threadinfo = {}
         self["render-default"] = "notmuch:threads"
-        self["line-format"] = "<%TM-hilite>%TM-date_relative</><tab:130></> <fg:blue>%+TM-authors</><tab:350>%.TM-threadinfo<tab:450><%TM-hilite>%.TM-subject</>                      "
+        self["line-format"] = "<%TM-hilite>%TM-date_relative</><tab:130></> <fg:blue>%TM-authors</><tab:350>%TM-threadinfo<tab:450><%TM-hilite>%TM-subject</>                      "
         self.add_notify(self.maindoc, "Notify:Tag")
         self.add_notify(self.maindoc, "Notify:Close")
         self.load_full()

render-format.c
@@ -16,27 +16,21 @@
 #include "core.h"
 #include "misc.h"
 
-struct rf_data {
-	int home_field;
-	int fields;
-};
 
-static char *do_format(struct rf_data *rf safe, struct pane *focus safe,
+static char *do_format(struct pane *focus safe,
 		       struct mark *m safe, struct mark *pm,
 		       int len, int attrs)
 {
 	char *body = pane_attr_get(focus, "line-format");
 	struct buf ret;
 	char *n;
-	int home = 0;
-	int field = 0;
 
 	if (pm && !mark_same(pm, m))
 		pm = NULL;
 	buf_init(&ret);
 
 	if (!body)
-		body = "%+name";
+		body = "%name";
 	n = body;
 	if (pm)
 		goto endwhile;
@@ -63,22 +57,12 @@ static char *do_format(struct rf_data *rf safe, struct pane *focus safe,
 			n += 1;
 			continue;
 		}
-		if (n[1] == '+' || n[1] == '.')
-			field += 1;
 
 		if (len >= 0 && ret.len >= len)
 			break;
 		if (pm)
 			break;
 		n += 1;
-		if (*n == '+') {
-			/* Home field */
-			n += 1;
-			home = field;
-			if (rf->home_field < 0)
-				rf->home_field = home;
-		} else if (*n == '.')
-			n += 1;
 		b = buf;
 		while (*n == '-' || *n == '_' || isalnum(*n)) {
 			if (b < buf + sizeof(buf) - 2)
@@ -133,11 +117,7 @@ static char *do_format(struct rf_data *rf safe, struct pane *focus safe,
 	}
 endwhile:
 	if (!*n) {
-		rf->fields = field+1;
-		rf->home_field = home;
-		if (len >= 0)
-			;
-		else
+		if (len < 0)
 			buf_append(&ret, '\n');
 	}
 	return buf_final(&ret);
@@ -145,8 +125,6 @@ endwhile:
 
 DEF_CMD(format_content)
 {
-	struct rf_data *rf = ci->home->data;
-
 	if (!ci->mark || !ci->comm2)
 		return Enoarg;
 
@@ -154,7 +132,7 @@ DEF_CMD(format_content)
 		const char *l, *c;
 		wint_t w;
 
-		l = do_format(rf, ci->focus, ci->mark, NULL, -1, 0);
+		l = do_format(ci->focus, ci->mark, NULL, -1, 0);
 		if (!l)
 			break;
 		c = l;
@@ -175,7 +153,6 @@ DEF_CMD(format_content)
 
 DEF_CMD(render_line)
 {
-	struct rf_data *rf = ci->home->data;
 	struct mark *m = ci->mark;
 	struct mark *pm = ci->mark2;
 	char *ret;
@@ -193,7 +170,7 @@ DEF_CMD(render_line)
 		len = -1;
 	else
 		len = ci->num;
-	ret = do_format(rf, ci->focus, ci->mark, pm, len, 1);
+	ret = do_format(ci->focus, ci->mark, pm, len, 1);
 	if (!pm && len < 0)
 		doc_next(ci->focus, m);
 	rv = comm_call(ci->comm2, "callback:render", ci->focus, 0, NULL, ret);
@@ -226,19 +203,6 @@ DEF_CMD(format_clone)
 	return 1;
 }
 
-DEF_CMD(format_get_attr)
-{
-	char attr[20];
-	struct rf_data *rf = ci->home->data;
-
-	if (!ci->mark ||
-	    !ci->str ||
-	    strcmp(ci->str, "markup:fields") != 0)
-		return 0;
-	sprintf(attr, "%u:%u", rf->home_field, rf->fields);
-	return comm_call(ci->comm2, "attr", ci->focus, 0, ci->mark, attr);
-}
-
 static struct map *rf_map;
 
 static void render_format_register_map(void)
@@ -248,24 +212,19 @@ static void render_format_register_map(void)
 	key_add(rf_map, "doc:render-line", &render_line);
 	key_add(rf_map, "doc:render-line-prev", &render_line_prev);
 	key_add(rf_map, "Clone", &format_clone);
-	key_add(rf_map, "doc:get-attr", &format_get_attr);
 	key_add(rf_map, "doc:content", &format_content);
-	key_add(rf_map, "Free", &edlib_do_free);
 }
 
 DEF_LOOKUP_CMD(render_format_handle, rf_map);
 
 static struct pane *do_render_format_attach(struct pane *parent, int nolines)
 {
-	struct rf_data *rf;
 	struct pane *p;
 
 	if (!rf_map)
 		render_format_register_map();
 
-	alloc(rf, pane);
-	rf->home_field = -1;
-	p = pane_register(parent, 0, &render_format_handle.c, rf);
+	p = pane_register(parent, 0, &render_format_handle.c);
 	attr_set_str(&p->attrs, "render-wrap", "no");
 	if (nolines)
 		return p;