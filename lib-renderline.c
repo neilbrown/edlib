neilbrown/edlib

commit 8202e0ece08ed7001f3123e6db9084f3c6030060

render-line: use helper-pane height to communicate render size.
Rather than passing back the render size with "render-done"
resize the pane so the caller can see that.
Also don't pass in a start line, but adjust y pos and height
to tell where to draw.

Signed-off-by: NeilBrown <neil@brown.name>

lib-renderline.c
@@ -420,7 +420,6 @@ DEF_CMD(renderline)
 	struct pane *p = ci->home;
 	struct pane *focus = ci->focus;
 	const char *line = ci->str;
-	short y_start = ci->num;
 	int dodraw = strcmp(ci->key, "render-line:draw") == 0;
 	short posx = ci->x;
 	short posy = ci->y;
@@ -428,7 +427,7 @@ DEF_CMD(renderline)
 	struct command *comm2 = ci->comm2;
 
 	int x = 0;
-	int y = y_start;
+	int y = 0;
 	const char *line_start;
 	const char *start;
 	struct buf attr;
@@ -768,7 +767,7 @@ DEF_CMD(renderline)
 			cy = cx = -1;
 		}
 	}
-	if (x > 0 || y == y_start)
+	if (x > 0 || y == 0)
 		/* No newline at the end .. but we must render as whole lines */
 		y += line_height;
 	free(buf_final(&attr));
@@ -777,8 +776,9 @@ DEF_CMD(renderline)
 		p->cx = cx;
 		p->cy = cy;
 	}
-	comm_call(comm2, "render-done", p, y, NULL, end_of_page ? "yes":NULL,
+	comm_call(comm2, "render-done", p, 0, NULL, end_of_page ? "yes":NULL,
 		  mwidth);
+	pane_resize(p, p->x, p->y, p->w, y);
 	while (rlst) {
 		struct render_list *r = rlst;
 		rlst = r->next;

render-lines.c
@@ -123,7 +123,6 @@ struct rl_data {
 	struct pane	*helper safe; /* pane where renderlines happens. */
 	struct command	c;
 	/* following set by measure/draw_line() callback */
-	short		y;
 	short		end_of_page;
 	short		xypos;	/* Where in line the x,y pos was found */
 	const char	*xyattrs;
@@ -135,7 +134,6 @@ DEF_CMD(rl_cb)
 	struct rl_data *rl = container_of(ci->comm, struct rl_data, c);
 
 	if (strcmp(ci->key, "render-done") == 0) {
-		rl->y = ci->num;
 		if (ci->str)
 			rl->end_of_page = 1;
 		rl->cwidth = ci->num2;
@@ -165,12 +163,13 @@ static void measure_line(struct pane *p safe, struct pane *focus safe,
 {
 	struct rl_data *rl = p->data;
 
+	pane_resize(rl->helper, 0, y_start, p->w, p->h - y_start);
 	pane_call(rl->helper,
 		  "render-line:measure",
 		  focus,
-		  y_start, NULL, mk->mdata ?: "",
+		  0, NULL, mk->mdata ?: "",
 		  offset, NULL, NULL,
-		  posx, posy, &rl->c);
+		  posx, posy < 0 ? posy : posy-y_start, &rl->c);
 }
 
 static void draw_line(struct pane *p safe, struct pane *focus safe,
@@ -178,10 +177,11 @@ static void draw_line(struct pane *p safe, struct pane *focus safe,
 {
 	struct rl_data *rl = p->data;
 
+	pane_resize(rl->helper, 0, y_start, p->w, p->h - y_start);
 	pane_call(rl->helper,
 		  "render-line:draw",
 		  focus,
-		  y_start, NULL, mk->mdata ?: "",
+		  0, NULL, mk->mdata ?: "",
 		  offset, NULL, NULL,
 		  -1, -1, &rl->c);
 	if (offset >= 0) {
@@ -374,10 +374,9 @@ static void find_lines(struct mark *pm safe, struct pane *p safe,
 	if (!end)
 		goto abort; /* FIXME can I prove this? */
 
-	rl->y = y = 0;
-	measure_line(p, focus, start, y, -1, -1, offset);
+	measure_line(p, focus, start, 0, -1, -1, offset);
 	//lines_above = rl->cy;
-	y = rl->y;
+	y = rl->helper->h;
 	lines_above = lines_below = 0;
 
 	/* We have start/end of the focus line, and its height.
@@ -413,9 +412,8 @@ static void find_lines(struct mark *pm safe, struct pane *p safe,
 				start = m;
 				if (!start->mdata)
 					call_render_line(focus, start, &end);
-				rl->y = 0;
-				measure_line(p, focus, start, h, -1, -1, -1);
-				h = rl->y;
+				measure_line(p, focus, start, 0, -1, -1, -1);
+				h = rl->helper->h;
 				if (h)
 					lines_above = h;
 				else
@@ -437,7 +435,7 @@ static void find_lines(struct mark *pm safe, struct pane *p safe,
 				short h;
 				rl->end_of_page = 0;
 				measure_line(p, focus, end, 0, -1, -1, -1);
-				h = rl->y;
+				h = rl->helper->h;
 				found_end = rl->end_of_page;
 				end = next;
 				if (h)
@@ -566,7 +564,7 @@ restart:
 			draw_line(p, focus, m2, 0, -1);
 			m2->mdata = NULL;
 			mark_free(m2);
-			y = rl->y;
+			y = rl->helper->h;
 		}
 		rl->header_lines = y;
 	}
@@ -592,7 +590,7 @@ restart:
 			rl->xypos = -1;
 			rl->cwidth = 1;
 			draw_line(p, focus, m, y, len);
-			y = rl->y;
+			y += rl->helper->h;
 			if (p->cy < 0)
 				p->cx = -1;
 			if (!rl->do_wrap && p->cy >= 0 &&
@@ -649,7 +647,7 @@ restart:
 			cursor_drawn = 1;
 		} else {
 			draw_line(p, focus, m, y, -1);
-			y = rl->y;
+			y += rl->helper->h;
 		}
 		if (!m2 || mark_same(m, m2))
 			break;
@@ -859,7 +857,7 @@ DEF_CMD(render_lines_move)
 					break;
 				}
 				measure_line(p, focus, m, y, -1, -1, -1);
-				y = rl->y;
+				y += rl->helper->h;
 				m = vmark_next(m);
 			}
 			rl->skip_lines = y;
@@ -877,7 +875,7 @@ DEF_CMD(render_lines_move)
 			if (rl->end_of_page)
 				y = rpt % pagesize;
 			else
-				y = rl->y;
+				y = rl->helper->h;
 			if (rl->skip_lines + rpt < y) {
 				rl->skip_lines += rpt;
 				break;
@@ -958,7 +956,7 @@ DEF_CMD(render_lines_set_cursor)
 		rl->xypos = -1;
 		rl->xyattrs = NULL;
 		measure_line(p, focus, m, y, cih.x, cih.y, -1);
-		y = rl->y;
+		y += rl->helper->h;
 		if (rl->xypos >= 0) {
 			struct mark *m2 = call_render_line_offset(focus, m,
 								  rl->xypos);
@@ -1123,7 +1121,7 @@ DEF_CMD(render_lines_move_line)
 	call_render_line(focus, start, NULL);
 	rl->xypos = -1;
 	measure_line(p, focus, start, y, rl->target_x, rl->target_y, -1);
-	y = rl->y;
+	y += rl->helper->h;
 	/* rl->xypos is the distance from start-of-line to the target */
 	if (rl->xypos >= 0) {
 		struct mark *m2 = call_render_line_offset(

commit b5f7914859034b21745baf9c6c4123e2e30a674f

lib-renderline: don't create extra help pane for images.
As lib-renderline now gets a pane-per-line, we don't need an extra
pane for image.

This also fixes a bug introduced recently which reported the size of an
image line as too large.

Fixes: 8202e0ece08e ("render-line: use helper-pane height to communicate render size.")
Signed-off-by: NeilBrown <neil@brown.name>

lib-renderline.c
@@ -347,13 +347,8 @@ static void update_line_height(struct pane *p safe, struct pane *focus safe,
 	free(buf_final(&attr));
 }
 
-DEF_CMD(null)
-{
-	return 0;
-}
-
-static int render_image(struct pane *p safe, struct pane *focus safe,
-			const char *line safe, short y,
+static void render_image(struct pane *p safe, struct pane *focus safe,
+			const char *line safe,
 			int dodraw, int scale)
 {
 	char *fname = NULL;
@@ -378,17 +373,11 @@ static int render_image(struct pane *p safe, struct pane *focus safe,
 		line += len;
 		line += strspn(line, ",");
 	}
-	if (fname && dodraw) {
-		struct pane *tmp = pane_register(p, -1, &null);
+	pane_resize(p, (p->w - width)/2, p->y, width, height);
+	if (fname && dodraw)
+		home_call(focus, "Draw:image", p, 0, NULL, fname, 5);
 
-		if (tmp) {
-			pane_resize(tmp, (p->w - width)/2, y, width, height);
-			home_call(focus, "Draw:image", tmp, 0, NULL, fname, 5);
-			pane_close(tmp);
-		}
-	}
 	free(fname);
-	return y + height;
 }
 
 static void find_xypos(struct render_list *rlst,
@@ -473,7 +462,7 @@ DEF_CMD(renderline)
 		 * something that makes sense.
 		 * The cursor is not on the image.
 		 */
-		y = render_image(p, focus, line, y, dodraw, scale);
+		render_image(p, focus, line, dodraw, scale);
 		attr_set_int(&p->attrs, "line-height", p->h);
 		p->cx = p->cy = -1;
 		return 1;