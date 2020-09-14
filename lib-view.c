efc23ceccf4b563d470e548d18b075f0caa71774
NeilBrown <neil@brown.name>
2020-09-14 20:51:39 +1000

    view: add doc render/view info
    I'm not sure if this is what I want, but it is worth experimenting.
    Display default-render/default-view in the status bar.
    
    This requires all tests to be refreshed.
    
    Signed-off-by: NeilBrown <neil@brown.name>

--- lib-view.c
+++ lib-view.c
@@ -45,7 +45,7 @@ static struct pane *do_view_attach(struct pane *par, int border);
 static int calc_border(struct pane *p safe);
 
 static char default_status[] =
-	"{!CountLines}M:{doc-modified?,*,-}{doc-readonly?,%%,  } D:{doc-file-changed?,CHANGED:,}{doc-name:-15} L{^line}/{lines} {doc-status}";
+	"{!CountLines}M:{doc-modified?,*,-}{doc-readonly?,%%,  } D:{doc-file-changed?,CHANGED:,}{doc-name:-15} L{^line}/{lines} {render-default}/{view-default} {doc-status}";
 static char default_title[] =
 	"{doc-name}";
 
