neilbrown/edlib

commit 8c766f74cfaf435b2323a0aa3a296558c1b728ae

render-lines: don't force find-lines() when appending to doc.
When we append to a doc, the point will move after the previously
displayed content.  This currently causes find_lines() to be required,
but that isn't really what we want.  We want to still keep the
top-of-display stable in that case.

We can detect it because m2->mdata will be non-NULL.  That only happens
when it is end-of-file.

Signed-off-by: NeilBrown <neil@brown.name>

render-lines.c
@@ -793,9 +793,13 @@ DEF_CMD(render_lines_revise)
 		call("doc:render-line-prev", focus, 0, m1);
 	// FIXME double check that we invalidate line before any change...
 
+	/* If m2->mdata is not NULL, then it was the end-of-file so
+	 * there could be blank space one display the new text can fill.
+	 * In that case we don't require pm to be before m2.
+	 */
 	if (m1 && m2 &&
 	    (!pm || (mark_ordered_or_same(m1,pm) &&
-		     mark_ordered_or_same(pm,m2)))) {
+		     (m2->mdata || mark_ordered_or_same(pm,m2))))) {
 		/* We maybe be able to keep m1 as start, if things work out.
 		 * So check all sub-panes are still valid and properly
 		 * positioned

tests.d/00-basic
@@ -18,15 +18,15 @@ Display 80,30 28D1C72D62F8E23B2783F54B768ABC99 1,0
 Key ":M->"
 Display 80,30 16581EB5FE2925B772D353935F30C8CC 1,25
 Key ":Enter"
-Display 80,30 2738FCE5BBFDCBFD39D938CD6935FFF9 1,25
+Display 80,30 4860F9199B6638A70D51AA0A8BA36495 1,26
 Key ":Backspace"
-Display 80,30 949D7E61F07C0114D3C2FE5B26ADDFA0 1,24
+Display 80,30 670521426AFCA22B9B2085A336F69772 1,25
 Key ":C-_"
-Display 80,30 2738FCE5BBFDCBFD39D938CD6935FFF9 1,25
+Display 80,30 4860F9199B6638A70D51AA0A8BA36495 1,26
 Key ":C-_"
-Display 80,30 C8929046FE147A5FBA00C812C7B67F94 1,24
+Display 80,30 16581EB5FE2925B772D353935F30C8CC 1,25
 Key ":C-X:C-x"
-Display 80,30 D3006E5EE5C57A59C542BAAED6788AF1 1,24
+Display 80,30 8014E19387B8703FBFED17D258C2DBCB 1,25
 Key ":C-V:C-v"
 Display 80,30 28D1C72D62F8E23B2783F54B768ABC99 1,0
 Key ":C-N:C-n"

commit 85ff390be3d6819e428e32fef89682c38e0ae829

render-lines: fix render_lines_revise() when appending to doc
The condition I added recently was exactly inverted.
Also add a test case to check this doesn't regress.

Fixes: 8c766f74cfaf ("render-lines: don't force find-lines() when appending to doc.")
Signed-off-by: NeilBrown <neil@brown.name>

render-lines.c
@@ -819,13 +819,13 @@ DEF_CMD(render_lines_revise)
 		call("doc:render-line-prev", focus, 0, m1);
 	// FIXME double check that we invalidate line before any change...
 
-	/* If m2->mdata is not NULL, then it was the end-of-file so
-	 * there could be blank space one display the new text can fill.
+	/* If m2->mdata is NULL, then it was the end-of-file so
+	 * there could be blank space on display the new text can fill.
 	 * In that case we don't require pm to be before m2.
 	 */
 	if (m1 && m2 &&
 	    (!pm || (mark_ordered_or_same(m1,pm) &&
-		     (m2->mdata || mark_ordered_or_same(pm,m2))))) {
+		     (m2->mdata == NULL || mark_ordered_or_same(pm,m2))))) {
 		/* We maybe be able to keep m1 as start, if things work out.
 		 * So check all sub-panes are still valid and properly
 		 * positioned

tests.d/01-redraw
@@ -1,0 +1,42 @@
+Display 80,30 48D200B55ACF87A3F33389A91A5D558A 1,1
+Key ":C-N"
+Display 80,30 27EB926B64D50D1989F541026CEE8F9B 1,2
+Key ":C-N"
+Display 80,30 50E2295B2D24D12C231595AEC77E3756 1,3
+Key ":C-N"
+Display 80,30 A8F5D01EAA448E943F3209323CCF5047 1,4
+Key ":C-N"
+Display 80,30 77E441DEF9BEFEF337BBF5B3C2F3AF47 1,5
+Key ":C-N"
+Display 80,30 F9628F1269287CDED80249287E1EB714 1,6
+Key ":C-N"
+Display 80,30 BE318F7BD343BAC1D1FE8C5EC72CE9DE 1,7
+Key ":C-N"
+Display 80,30 4150511C4B7285F0E7064EBE8B658CC7 1,8
+Key "-f"
+Display 80,30 90DB7CA40C572A8A494CA64CD269C84B 1,0
+Key ":A->"
+Display 80,30 94E999E9995A7B3B728422A0F7E7E9F3 1,25
+Key ":Backspace"
+Display 80,30 987CFF044EC34EBFB80087BE3C970249 1,24
+Key ":Backspace"
+Display 80,30 DD44EC05B66FA549470E84E8EE94ADEC 1,23
+Key ":Backspace"
+Display 80,30 5505C3DB769AD92D05500B417C706970 58,22
+Key ":C-L"
+Display 80,30 093DEEBAE6D854B2D36D45E78254D0E8 58,25
+Key ":C-L"
+Display 80,30 B4D2928F0ADB123A70019EF6F9CA788A 58,0
+Key ":A->"
+Display 80,30 9477374E6199F032194C0E2FD9B5298D 58,0
+Key ":Enter"
+Display 80,30 65D114079251A92DAC298D6B44F53BC3 1,1
+Key ":C-X"
+Display 80,30 1458F1BE4E1AA1AB5FC8DCE7544B7B37 1,1
+Key "-k"
+Display 80,30 4150511C4B7285F0E7064EBE8B658CC7 1,8
+Key ":C-X"
+Display 80,30 E7E1B54A11B68FCCA8F7D69F1C3252AE 1,8
+Key ":C-C"
+Display 80,30 4150511C4B7285F0E7064EBE8B658CC7 1,8
+Close