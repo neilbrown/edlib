8230fddc172f0c6863f5771d614c727369d31153
NeilBrown <neil@brown.name>
2021-03-24 14:54:23 +1100

    notmuch: doc:step returns a char
    When we call variants of doc:step and check the return value, we need to
    specify that we expect a 'char'.
    
    Signed-off-by: NeilBrown <neil@brown.name>

--- python/module-notmuch.py
+++ python/module-notmuch.py
@@ -1448,13 +1448,15 @@ class notmuch_query_view(edlib.Pane):
                 if mark >= self.thread_end:
                     ret = edlib.WEOF
                 else:
-                    ret = self.parent.call("doc:step", focus, forward, move, mark)
+                    ret = self.parent.call("doc:step", focus, forward, move,
+                                           mark, ret='char')
             else:
                 if mark <= self.thread_start:
                     # at start already
                     ret = edlib.WEOF
                 else:
-                    ret = self.parent.call("doc:step", focus, forward, move, mark)
+                    ret = self.parent.call("doc:step", focus, forward, move,
+                                           mark, ret='char')
             return ret
         else:
             # if between thread_start/thread_end, move one message,
@@ -1473,7 +1475,8 @@ class notmuch_query_view(edlib.Pane):
                 in_thread = True
             if in_thread:
                 # move one matched message
-                ret = self.parent.call("doc:step-matched", focus, mark, forward, move)
+                ret = self.parent.call("doc:step-matched", focus, mark, forward,
+                                       move, ret='char')
                 # If moving forward, we might be in the next thread,
                 # make sure we didn't go further than thread_end
                 if forward and move and mark > self.thread_end:
@@ -1482,11 +1485,13 @@ class notmuch_query_view(edlib.Pane):
             else:
                 # move one thread
                 if forward:
-                    ret = focus.call("doc:step-thread", focus, mark, forward, move)
+                    ret = focus.call("doc:step-thread", focus, mark, forward,
+                                     move, ret='char')
                     if self.thread_start and mark == self.thread_start:
                         mark.to_mark(self.thread_matched)
                 else:
-                    ret = self.parent.call("doc:step", focus, mark, forward, move)
+                    ret = self.parent.call("doc:step", focus, mark, forward,
+                                           move, ret='char')
                     if move and ret != edlib.WEOF:
                         focus.call("doc:step-thread", focus, mark, forward, move)
                 return ret
