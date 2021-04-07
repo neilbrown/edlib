284d0341fc1f9e461e9a152462241ca18fbaf8f4
NeilBrown <neil@brown.name>
2021-04-08 07:11:18 +1000

    core.h: fix error in is_eol()
    I create a local var, and don't use it consistently!!
    
    Signed-off-by: NeilBrown <neil@brown.name>

--- core.h
+++ core.h
@@ -161,7 +161,7 @@ void log_setup(struct pane *ed safe);
 
 #define CHAR_RET(_c) ((_c & 0xFFFFF) | 0x100000)
 
-#define is_eol(c) ({int __c = c; __c == '\n' || c == '\v'; })
+#define is_eol(c) ({int __c = c; __c == '\n' || __c == '\v'; })
 
 /* Points and Marks */
 
