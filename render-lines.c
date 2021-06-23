b4699c2653733a0f5e8dcf1331afdfcb085a1741
NeilBrown <neil@brown.name>
2021-06-24 08:24:20 +1000

    render-lines: check for negative returns from call.
    A negative return from call is a bad (in most cases) as zero.
    So don't use "!call()".
    
    Signed-off-by: NeilBrown <neil@brown.name>

--- render-lines.c
+++ render-lines.c
@@ -1464,13 +1464,13 @@ DEF_CMD(render_lines_move_line)
 		num -= 1;
 	else
 		num += 1;
-	if (!call("doc:EOL", ci->focus, num, m)) {
+	if (call("doc:EOL", ci->focus, num, m) <= 0) {
 		rl->i_moved = 0;
 		return Efail;
 	}
 	if (RPT_NUM(ci) > 0) {
 		/* at end of target line, move to start */
-		if (!call("doc:EOL", ci->focus, -1, m)) {
+		if (call("doc:EOL", ci->focus, -1, m) <= 0) {
 			rl->i_moved = 0;
 			return Efail;
 		}
