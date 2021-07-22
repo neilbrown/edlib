neilbrown/edlib

commit d9812b20bea880fe3f1aa002e327be7993c5c67f

ncurses: receive tty/term as str1/str2, not on parent pane or env
Setting TERM on the parent pane before starting a display is a bit
weird, and won't work with an upcoming patch.  So pass the TERM
in as str2.
Also set other env vars on the pane, not on its parent.

Signed-off-by: NeilBrown <neil@brown.name>

display-ncurses.c
@@ -1002,7 +1002,7 @@ static struct pane *ncurses_init(struct pane *ed,
 	FILE *f;
 
 	set_screen(NULL);
-	if (tty)
+	if (tty && strcmp(tty, "-") != 0)
 		f = fopen(tty, "r+");
 	else
 		f = fdopen(1, "r+");
@@ -1354,15 +1354,13 @@ REDEF_CMD(input_handle)
 DEF_CMD(display_ncurses)
 {
 	struct pane *p;
-	char *term;
+	const char *tty = ci->str;
+	const char *term = ci->str2;
 
-	term = pane_attr_get(ci->focus, "TERM");
-	if (!term)
-		term = getenv("TERM");
 	if (!term)
 		term = "xterm-256color";
 
-	p = ncurses_init(ci->focus, ci->str, term);
+	p = ncurses_init(ci->focus, tty, term);
 	if (p) {
 		struct pane *p2 = call_ret(pane, "attach-x11selection", p);
 		if (p2)

edlib.c
@@ -140,15 +140,16 @@ int main(int argc, char *argv[])
 		struct pane *disp = NULL;
 		p = call_ret(pane, "attach-input", ed);
 		if (p) {
-			char *e;
-			attr_set_str(&p->attrs, "TERM", getenv("TERM"));
-			e = getenv("SSH_CONNECTION");
-			if (e && *e)
-				attr_set_str(&p->attrs, "REMOTE_SESSION", "yes");
-			disp = call_ret(pane, "attach-display-ncurses",
-					p);
+			char *TERM = getenv("TERM");
+			disp = call_ret(pane, "attach-display-ncurses", p,
+					0, NULL, "-", 0, NULL, TERM);
 		}
 		if (disp) {
+			char *e;
+			e = getenv("SSH_CONNECTION");
+			if (e && *e)
+				attr_set_str(&disp->attrs, "REMOTE_SESSION", "yes");
+
 			attr_set_str(&disp->attrs, "DISPLAY", getenv("DISPLAY"));
 			p = make_stack(disp, doc);
 			if (p && !first_window)

python/lib-server.py
@@ -105,14 +105,18 @@ try:
                     path = w[1].decode("utf-8")
                     p = editor.call("attach-input", ret='pane')
 
+                    env={}
                     for v in w[2:]:
                         vw = v.split(b'=')
                         if len(vw) == 2 and vw[0] in [b'TERM',
                                                       b'DISPLAY',
                                                       b'REMOTE_SESSION']:
-                            p[vw[0].decode("utf-8")] = vw[1].decode("utf-8")
+                            env[vw[0].decode("utf-8")] = vw[1].decode("utf-8")
 
-                    p = p.call("attach-display-ncurses", path, ret='pane')
+                    p = p.call("attach-display-ncurses", path, env['TERM'],
+                               ret='pane')
+                    for v in env:
+                        p[v] = env[v]
                     self.disp = p
                     p = p.call("attach-messageline", ret='pane')
                     p = p.call("attach-global-keymap", ret='pane')

commit 320ddf49c4dcd02b91a2db6451a106adc6a2c10a

ncurses: also catch SIGWINCH if tty is "-"
Currently SIGWINCH is only caught is 'tty' is NULL, but a recent change
allowed it to be "-" for the "stdin" case.  Oops.

Fixes: d9812b20bea8 ("ncurses: receive tty/term as str1/str2, not on parent pane or env")
Signed-off-by: NeilBrown <neil@brown.name>

display-ncurses.c
@@ -1063,7 +1063,7 @@ static struct pane *ncurses_init(struct pane *ed,
 	call("editor:request:all-displays", p);
 	if (!prepare_recrep(p)) {
 		call_comm("event:read", p, &input_handle, fileno(f));
-		if (!tty)
+		if (!tty || strcmp(tty, "-") == 0)
 			call_comm("event:signal", p, &handle_winch, SIGWINCH);
 	}
 	return p;