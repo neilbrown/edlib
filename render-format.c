126d07e99fd483bffe67e7f54c0c0ea9939c26e7
NeilBrown <neil@brown.name>
2022-05-28 10:36:25 +1000

    Update the year in some copyright claims
    It is well into 2022 now, so time to update some copyright statements.
    
    Signed-off-by: NeilBrown <neil@brown.name>

--- render-format.c
+++ render-format.c
@@ -1,5 +1,5 @@
 /*
- * Copyright Neil Brown ©2015-2021 <neil@brown.name>
+ * Copyright Neil Brown ©2015-2022 <neil@brown.name>
  * May be distributed under terms of GPLv2 - see file:COPYING
  *
  * render-format.  Provide 'render-line' functions to render
