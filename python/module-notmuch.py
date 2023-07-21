neilbrown/edlib

commit 5707b369a802105f79c789383ee7d82253669ee8

notmuch: when moving among messages, set focus to query pane.
Leaving the focus in the message pane feels clumsy.  So let's try always
moving back to the query pane.

Signed-off-by: NeilBrown <neil@brown.name>

python/module-notmuch.py
@@ -2033,6 +2033,8 @@ class notmuch_master_view(edlib.Pane):
 
         m = p.call("doc:dup-point", 0, edlib.MARK_UNGROUPED, ret='mark')
         p.call("notmuch:select", m, direction)
+        if self.query_pane:
+            self.query_pane.take_focus()
         return 1
 
     def handle_j(self, key, focus, **a):

commit 24c5535900cc8ab0debedd269ed83eb9af900e3b

notmuch: focus on query pane when changing messages
The previous patch didn't fix this properly.  'K-a' wasn't handled for
examine.
Try again.

Fixes: 5707b369a802 ("notmuch: when moving among messages, set focus to query pane.")
Signed-off-by: NeilBrown <neil@brown.name>

python/module-notmuch.py
@@ -2038,8 +2038,6 @@ class notmuch_master_view(edlib.Pane):
 
         m = p.call("doc:dup-point", 0, edlib.MARK_UNGROUPED, ret='mark')
         p.call("notmuch:select", m, direction)
-        if self.query_pane:
-            self.query_pane.take_focus()
         return 1
 
     def handle_j(self, key, focus, **a):
@@ -3055,6 +3053,7 @@ class notmuch_query_view(edlib.Pane):
                 focus.call("notmuch:select-message", s2, s)
                 self.selmsg = s2
                 self.call("view:changed")
+        self.take_focus()
         return 1
 
     def handle_reposition(self, key, focus, mark, mark2, **a):